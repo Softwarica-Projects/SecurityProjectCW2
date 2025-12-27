a5b9b5b6737c1fa77e59fd48d5bf6494d9be769c
Rishan <rishan.shrestha@proshore.eu>
2025-12-27 10:21:54 +0545

    added missing auth routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
 const path = require('path');
 const connectDB = require('./config/db');
 const app = express();
