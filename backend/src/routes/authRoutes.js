795f342e0efbbf9234949eeafaadf21056f79f5c
Rishan <rishan.shrestha@proshore.eu>
2025-12-27 10:10:35 +0545

    added rate limiter and included the rate limiter for authentication

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { authenticateToken } = require('../middlewares/authMiddleware');
 const authController = require('../controllers/authController');
+const { authLimiter } = require('../middlewares/rateLimitMiddleware');
 
 const router = express.Router();
 
-router.post('/register', authController.registerUser);
+router.post('/register', authLimiter, authController.registerUser);
 
-router.post('/login', authController.loginUser);
+router.post('/login', authLimiter, authController.loginUser);
 
-router.post('/change-password', authenticateToken, authController.changePassword);
+router.post('/change-password', authenticateToken, authLimiter, authController.changePassword);
 
 router.get('/me', authenticateToken, authController.getMe);
 
