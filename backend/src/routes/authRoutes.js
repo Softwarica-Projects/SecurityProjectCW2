df90eb1112c2e8321fb19b329a3436dbf37fe3d8
Rishan <shrestha.rishan99@gmail.com>
2026-01-19 07:37:45 +0545

    added update profile route which was missing the api route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -14,5 +14,6 @@ router.post('/change-password', authenticateToken, authLimiter, authController.c
 router.get('/me', authenticateToken, authLimiter,authController.getMe);
 router.get('/stats', authenticateToken, authLimiter,authController.getUserStats);
 router.get('/favorites', authenticateToken, authLimiter,authController.getFavoriteMovies);
+router.put('/update-profile', authenticateToken, authLimiter,authController.updateProfile);
 
 module.exports = router;
